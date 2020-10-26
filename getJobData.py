5a8c5a9144f579987d6350ab694231056d6e8b5f
t2foley <t2foley@student.bridgew.edu>
2020-10-26 19:53:49 -0400

    added getData and displayData functions

--- getJobData.py
+++ getJobData.py
@@ -1,6 +1,16 @@
-def main():
+import requests
+
+def displayData(toDisplay):
     pass
 
+def getData():
+    pass
+
+
+def main():
+    data = getData()
+
+    displayData(data)
 
 if __name__ == '__main__':
     main()
